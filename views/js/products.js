d45f0a362e2ba7f4a6c115030a2e0284352a819e
tango025 <gaurav2550.gg@gmail.com>
2019-02-01 23:43:01 +0530

    some bugs in update

---
+++ views/js/products.js
@@ -0,0 +1,92 @@
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+var updateText = document.getElementById('updateText');
+var productObject = {};
+var uid,produpdate,userupdate;
+function update(product) {
+    productObject = createObject(product);
+    //add innerhtml to updateText
+    modal.style.display = "block";
+    var text = "<form>";
+    for (key in productObject) {
+        if (key != 'URL' && key!='CATEGORY')
+            text += '<label class = "tag">' + key + '</label><input type = "text" id ="' + key + '" value = "' + productObject[key] + '"><br>';
+    }
+    text += '<input type="button" onclick ="fireUpdate()" value ="Update"> <input type = "button" onclick = "cancel()" value ="Cancel"></form>';
+    updateText.innerHTML = text;
+
+};
+
+function fireUpdate() {
+    var localObj = {};
+    for (key in productObject) {
+        if (key != 'URL'&& key != 'CATEGORY') {
+            var changedVal = document.getElementById(key).value;
+            if (changedVal != productObject[key])
+                localObj[key] = changedVal;
+        }
+    }
+    var uid = firebase.auth().currentUser.uid;
+    var produpdate = "categories/" + productObject.CATEGORY + "/" + productObject.NAME;
+    var userupdate = "sellers/seller_wise/" + uid + "/" + productObject.CATEGORY + "/" + productObject.NAME;
+    firebase.database().ref().child(userupdate).update(localObj);
+    firebase.database().ref().child(produpdate).update(localObj,function(err){
+        modal.style.display = "none";
+        //remove innerhtml also
+        document.getElementById('updateText').innerHTML = "";
+        location.reload();
+    })
+   
+}
+//close btn for modal
+span.onclick = function () {
+    modal.style.display = "none";
+    //remove innerhtml also
+    document.getElementById('updateText').innerHTML = "";
+}
+//close modal if click outside screen
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        //remove innerhtml also
+        document.getElementById('updateText').innerHTML = "";
+    }
+}
+function cancel() {
+    modal.style.display = "none";
+    //remove innerhtml also
+    document.getElementById('updateText').innerHTML = "";
+}
+function createObject(productArray) {
+    var localArrKeys = [];
+    var localArrVal = [];
+    var object = {};
+    var separated = productArray.split(",");
+    // console.log(separated);
+    //storings keys in separated array
+    object['CATEGORY'] = separated.shift();
+    for (var i = 0; i < separated.length; i++) {
+        if (separated[i].charAt(0) == "k")
+            localArrKeys.push(separated[i].slice(1));
+    }
+    //removing those keys from original array
+    for (var i = 0; i < localArrKeys.length; i++)
+        separated.shift();
+    for (var i = 0; i < separated.length; i++) {
+
+        if (separated[i].charAt(0) != "a") {
+            var j = i;
+            while (separated[j].charAt(0) != "a") {
+                separated[i - 1] += "," + separated[j];
+                separated.splice(j, 1)
+            }
+
+        }
+        if (separated[i].charAt(0) == "a") {
+            separated[i] = separated[i].slice(1);
+        }
+    }
+    for (var i = 0; i < separated.length; i++)
+        object[localArrKeys[i]] = separated[i];
+    return object;
+};
